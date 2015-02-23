8ff5dc7c4285062de8a422a8f4be3dfec95e351e
dswalter <dswalter@gmail.com>
2015-02-23 14:05:26 -0500

    remove just_Rcmdr

--- stats250/R/Rcmdr.R
+++ stats250/R/Rcmdr.R
@@ -1,21 +1,8 @@
 #' Load R Commander with the time series R Commander Plugin.
 #' This way, run Rcmdr() and the environment will be ready.
-#'
+#' @import RcmdrPlugin.epack
 #' @examples
 #' Rcmdr()
 Rcmdr<-function(){
-  library("RcmdrPlugin.epack",quietly = TRUE)
   Rcmdr::Commander()
 }
-
-
-#' Load R Commander alone. This way, run Rcmdr()
-#' and the environment will be ready.
-#'
-#' @examples
-#' just_Rcmdr()
-just_Rcmdr<-function(){
-  library("Rcmdr",quietly = TRUE)
-  Rcmdr::Commander()
-}
-
