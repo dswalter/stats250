a5873459c826d01bcd8cbfdf7d4287d9eabe2447
dswalter <dswalter@gmail.com>
2015-02-26 15:42:54 -0500

    rename and update

---
+++ learnstats/R/learnstats.R
@@ -0,0 +1,15 @@
+#' learnstats: Using R as an interactive educational environment.
+#'
+#'
+#' @docType package
+#' @name stats250
+NULL
+
+#' @import Rcmdr
+NULL
+
+#' @import RcmdrPlugin.epack
+NULL
+
+#' @import ggplot2
+NULL
